2d162d9c974697d01d1087a669a2622c78423e49
Fuzailkazi <mohdfuzailkazi14@gmail.com>
2025-01-04 21:56:41 +0530

    shadcn init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
+import { Button } from '@/components/ui/button';
 import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs';
 
 export default function Home() {
   return (
-    <>
+    <div className='m-4'>
       <SignedOut>
-        <SignInButton />
+        <SignInButton mode='modal'>
+          <Button>SIgn In</Button>
+        </SignInButton>
       </SignedOut>
       <SignedIn>
         <UserButton />
       </SignedIn>
-    </>
+      <Button>Log out</Button>
+    </div>
   );
 }
