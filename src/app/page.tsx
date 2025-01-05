374848dfbe24ed51caa256fb5ca7bf5a4e991179
Fuzailkazi <mohdfuzailkazi14@gmail.com>
2025-01-05 15:11:54 +0530

    desktop navbar done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,16 +5,7 @@ import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs';
 export default function Home() {
   return (
     <div className='m-4'>
-      <SignedOut>
-        <SignInButton mode='modal'>
-          <Button>SIgn In</Button>
-        </SignInButton>
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-      <ModeToggle />
-      <Button>Log out</Button>
+      <h1>content page</h1>
     </div>
   );
 }
