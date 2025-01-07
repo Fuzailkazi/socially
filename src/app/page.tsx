5a99c09aa30b21a83753d22328766a9f5c635a6e
Fuzailkazi <mohdfuzailkazi14@gmail.com>
2025-01-07 13:21:07 +0530

    auth & db sync first

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import ModeToggle from '@/components/ModeToggle';
 import { Button } from '@/components/ui/button';
 import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs';
+import prisma from '@/lib/prisma';
 
 export default function Home() {
   return (
