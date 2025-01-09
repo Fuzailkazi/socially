a5ae635ca7e72e34e97b1ad87c2598687f8c809a
Fuzailkazi <mohdfuzailkazi14@gmail.com>
2025-01-09 22:12:58 +0530

    file name updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,14 @@ import ModeToggle from '@/components/ModeToggle';
 import { Button } from '@/components/ui/button';
 import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs';
 import prisma from '@/lib/prisma';
+import CreatePost from '@/components/CreatePost';
 import { currentUser } from '@clerk/nextjs/server';
 
-export default function Home() {
+export default async function Home() {
   const user = await currentUser();
   return (
     <div className='grid grid-cols-1 lg:grid-cols-10 gap-6'>
-      <div className='lg:col-span-6'>
-        <CreatePost />
-      </div>
+      <div className='lg:col-span-6'>{user ? <CreatePost /> : null}</div>
       <div className='hidden lg:block lg:col-span-4 sticky top-4'>
         WhoToFollow
       </div>
