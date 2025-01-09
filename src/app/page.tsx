939d0a421eb35381ee4f62a42abca8b7a4537e30
Fuzailkazi <mohdfuzailkazi14@gmail.com>
2025-01-09 21:58:18 +0530

    home layout updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,18 @@ import ModeToggle from '@/components/ModeToggle';
 import { Button } from '@/components/ui/button';
 import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs';
 import prisma from '@/lib/prisma';
+import { currentUser } from '@clerk/nextjs/server';
 
 export default function Home() {
+  const user = await currentUser();
   return (
-    <div className='m-4'>
-      <h1>content page</h1>
+    <div className='grid grid-cols-1 lg:grid-cols-10 gap-6'>
+      <div className='lg:col-span-6'>
+        <CreatePost />
+      </div>
+      <div className='hidden lg:block lg:col-span-4 sticky top-4'>
+        WhoToFollow
+      </div>
     </div>
   );
 }
