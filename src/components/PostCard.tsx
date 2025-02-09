4965bc9bb04798bc1f4c7bc3ac538e7894ae9127
Fuzailkazi <mohdfuzailkazi14@gmail.com>
2025-02-09 13:00:00 +0530

    notification actions

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,6 +1,37 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
+// import { Post } from '@prisma/client';
+import { useUser } from '@clerk/nextjs';
+import { getPosts } from '@/actions/post.action';
 
-const PostCard = () => {
+type Posts = Awaited<ReturnType<typeof getPosts>>;
+type Post = Posts[number];
+const PostCard = ({
+  post,
+  dbUserId,
+}: {
+  post: Post;
+  dbUserId: string | null;
+}) => {
+  const { user } = useUser();
+  const [newComment, setNewComment] = useState('');
+  const [isCommenting, setIsCommenting] = useState(false);
+  const [isLiking, setIsLiking] = useState(false);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [hasLiked, setHasLiked] = useState(false);
+  const [optimisticLikes, setOptimisticLikes] = useState(post._count.likes);
+  const handleLike = async () => {
+    if (isLiking) return;
+    try {
+      setIsLiking(true);
+      setHasLiked((prev) => !prev);
+      setOptimisticLikes((prev) => prev + (hasLiked ? -1 : +1));
+    } catch (error) {
+      setOptimisticLikes(post._count.likes);
+      setHasLiked(post.likes.some((like) => like.userId === dbUserId));
+      console.log(error);
+    }
+  };
   return <div>PostCard</div>;
 };
 
